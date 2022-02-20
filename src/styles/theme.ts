f6c0f73b3a3e89c57f65aa8649d27ecfac66b244
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2022-02-20 02:25:07 -0300

    feat: configure styled components typing and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+  main: {
+    colors: {
+      primary: '#2939C3',
+      secondary: '#EDEEF5',
+      white: '#FFFFFF',
+      gray01_bg: '#F6F6F7',
+      gray02: '#EBEBEC',
+      gray03: '#C2C3CA',
+      gray04: '#9699A2',
+      gray05: '#5D606D',
+      black: '#primary'
+    },
+    fonts: {
+      bold: 'Roboto-Bold',
+      medium: 'Roboto-Medium',
+      semi_bold: 'Roboto-SemiBold',
+      regular: 'Roboto-Regular'
+    }
+  }
+};
