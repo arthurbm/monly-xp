983d0bc3a74f94c9f925f77f4fb835d69e861143
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2022-02-19 22:35:01 -0300

    feat: first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo>Boilerplate</S.Logo>
+    <S.Title>Next.Js Boilerplate</S.Title>
+    <S.Description>
+      Next.Js, React.Js, Typescript e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor sentado a frente de um computador"
+    />
+  </S.Wrapper>
+)
+
+export default Main
