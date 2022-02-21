05543cc92266b3ecf5c71a9c2b57d2809274b1a7
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2022-02-20 23:34:52 -0300

    feat: make integration with suitability

--- src/components/FieldCheck/index.tsx
+++ src/components/FieldCheck/index.tsx
@@ -18,6 +18,7 @@ type FieldCheckProps = {
   description?: string;
   height?: string;
   usesEmoji?: boolean;
+  isRecommended?: boolean;
 };
 const FieldCheck = ({
   emoji,
@@ -27,18 +28,28 @@ const FieldCheck = ({
   checked,
   description,
   height,
-  usesEmoji
+  usesEmoji,
+  isRecommended
 }: FieldCheckProps) => {
+  const productDescriptionParsed = description?.split(';');
+
   return (
     <Container height={height}>
       <Info usesEmoji={usesEmoji}>
+        {isRecommended && (
+          <CustomText regular margin="0 0 10px 0">
+            Melhor opção
+          </CustomText>
+        )}
         {emoji && <CustomText regular>{emoji}</CustomText>}
         <CustomText regular black size="16px">
           {children}
         </CustomText>
-        <CustomText align="left" regular grey>
-          {description}
-        </CustomText>
+        {productDescriptionParsed?.map((productDescription) => (
+          <CustomText key={productDescription} align="left" regular grey>
+            {productDescription}
+          </CustomText>
+        ))}
       </Info>
 
       {comingSoon ? (
